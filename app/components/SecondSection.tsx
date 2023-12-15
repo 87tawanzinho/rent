6aada87b48b56c21fc2ddac9026f9831e74964b2
87tawanzinho <87tawanzinho@gmail.com>
2023-12-15 23:00:52 +0000

    change second div

--- app/components/SecondSection.tsx
+++ app/components/SecondSection.tsx
@@ -5,7 +5,7 @@ import graphic from '@/public/images/graphic.gif';
 import Typography from '@mui/material/Typography';
 export default function SecondSection() {
   const url =
-    'https://images.unsplash.com/photo-1570126646281-5ec88111777f?q=80&w=2041&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+    'https://images.unsplash.com/photo-1492144534655-ae79c964c9d7?q=80&w=1966&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
   return (
     <Box
       sx={{
@@ -13,23 +13,28 @@ export default function SecondSection() {
         flexDirection: { xs: 'column', lg: 'row' },
         justifyContent: 'center',
         alignItems: 'center',
-        background: 'black',
+        backgroundImage: `url(${url})`,
+
         textAlign: 'center',
         gap: '4rem',
         height: '100vh',
-
-        width: '100%'
+        filter: `grayscale(100%)`,
+        transition: 'filter 0.3s ease-in-out',
+        width: '100%',
+        ':hover': {
+          filter: `grayscale(50%)`
+        }
       }}
     >
-      <Box>
-        <Image src={timer} alt="timer" width={300} />
+      <Box sx={{ zIndex: 5 }}>
+        <Image src={timer} alt="timer" width={200} />
 
         <Typography variant="h6" sx={{ width: { xs: '80vw', lg: '400px', color: 'gray', background: 'white' } }}>
-          Pare de perder tempo e alugue um belo carro
+          Pare de perder tempo e escolha um belo carro
         </Typography>
       </Box>
       <Box>
-        <Image src={graphic} alt="graphic" width={300} />
+        <Image src={graphic} alt="graphic" width={200} />
         <Typography sx={{ width: { xs: '80vw', lg: '400px', color: 'gray', background: 'white' } }} variant="h6">
           São diversas pesquisas que comprovam que alugar sai mais barato que comprar!
         </Typography>
